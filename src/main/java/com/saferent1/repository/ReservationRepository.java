2d39a7a6a1aff66c31126127784a7d8fba5cde98
OmerCeleb <omer534@outlook.com>
2023-07-03 19:14:28 +0200

    Hyra Bil

--- src/main/java/com/saferent1/repository/ReservationRepository.java
+++ src/main/java/com/saferent1/repository/ReservationRepository.java
@@ -1,11 +1,28 @@
 package com.saferent1.repository;
 
 import com.saferent1.domain.Reservation;
+import com.saferent1.domain.enums.ReservationStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Repository
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
 
 
+    @Query("SELECT r FROM Reservation r " +
+            "JOIN FETCH Car c on r.car=c.id WHERE " +
+            "c.id=:carId and (r.status not in :status) and : pickUpTime BETWEEN r.pickUpTime and r.dropOfTime " +
+            "or " +
+            "c.id=:carId and (r.status not in :status) and dropOfTime BETWEEN r.pickUpTime and r.dropOfTime " +
+            "or " +
+            "c.id=:carId and (r.status not in :status) and (r.pickUpTime BETWEEN :pickUpTime and :dropOfTime)")
+    List<Reservation> checkCarStatus(@Param("carId") Long carId,
+                                     @Param("pickUpTime") LocalDateTime pickUpTime,
+                                     @Param("dropOfTime") LocalDateTime dropOfTime,
+                                     @Param("status") ReservationStatus[] status);
 }
--- src/main/java/com/saferent1/service/ReservationService.java
+++ src/main/java/com/saferent1/service/ReservationService.java
@@ -1,7 +1,9 @@
 package com.saferent1.service;
 
 import com.saferent1.domain.Car;
+import com.saferent1.domain.Reservation;
 import com.saferent1.domain.User;
+import com.saferent1.domain.enums.ReservationStatus;
 import com.saferent1.dto.request.ReservationRequest;
 import com.saferent1.exception.BadRequestException;
 import com.saferent1.exception.message.ErrorMessage;
@@ -9,6 +11,7 @@ import com.saferent1.repository.ReservationRepository;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 public class ReservationService {
@@ -41,17 +44,32 @@ public class ReservationService {
 
         if (isEqual || !isBefore) {
             throw new BadRequestException(ErrorMessage.RESERVATION_TIME_INCORRECT_MESSAGE);
+
         }
-
-
 
     }
 
     //!!! Är fordonet tillgängligt?
 
+
     // !!! prisberäkning
 
     // !!! connflict?
+    private List<Reservation> getConflictReservation(Car car, LocalDateTime pickUpTime,
+                                                     LocalDateTime dropOfTime) {
+
+        if (pickUpTime.isAfter(dropOfTime)) {
+            throw new BadRequestException(ErrorMessage.RESERVATION_TIME_INCORRECT_MESSAGE);
+        }
+
+        ReservationStatus[] status = {ReservationStatus.CANCELED, ReservationStatus.DONE};
+
+        List<Reservation> existReservation =
+                reservationRepository.checkCarStatus(car.getId(), pickUpTime, dropOfTime, status);
+
+        return existReservation;
+
+    }
 
 
 }
